b7a48af360bbcf61a917e344e60c46d7516782f1
dmitei <dima9990860349@icloud.com>
2025-10-21 20:25:12 +0300

    main props added

--- shabalin.dmitrii/F0/main.cpp
+++ shabalin.dmitrii/F0/main.cpp
@@ -1,6 +1,28 @@
+#include <limits>
 #include <iostream>
+#include "commands.hpp"
+#include "DictionaryCollection.hpp"
 
 int main()
 {
-  return 0;
+  using namespace shabalim;
+
+  DictionaryCollection collection{};
+  CommandTable commands;
+  getCommands(commands, collection);
+  std::string command;
+  while (!(std::cin >> command).eof())
+  {
+    try
+    {
+      commands.at(command)();
+    }
+    catch (const std::exception &e)
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+      std::cout << "<INVALID COMMAND>\n";
+      std::cout << e.what();
+    }
+  }
 }
