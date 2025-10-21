1d66792d44ac7be6149e8329f927938a8988121d
dmitei <dima9990860349@icloud.com>
2025-10-21 20:30:15 +0300

    decl added

---
+++ shabalin.dmitrii/F0/declarations.hpp
@@ -0,0 +1,15 @@
+#ifndef DECLARATIONS_HPP
+#define DECLARATIONS_HPP
+
+namespace shabalin
+{
+  template < class Key, class Value, class Hash, class Equal >
+  struct HashTable;
+
+  template < class Key, class Value, class Hash, class Equal >
+  struct HashIterator;
+
+  template < class Key, class Value, class Hash, class Equal >
+  struct ConstHashIterator;
+}
+#endif
